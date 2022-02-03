f294eb948785e9cb3cfe1e6fe6a17615e66ed5d2
Metin Ağaoğlu <metnagaoglu@gmail.com>
2022-02-03 15:28:42 +0300

    Login endpoint has been added

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const User = require('../models/User');
 const brcypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 router.post('/register',(req,res)=>{
     //TODO: validation
@@ -23,8 +24,27 @@ router.post('/register',(req,res)=>{
     });
 });
 
-router.post('/login',(req,res)=>{
-    // ..code
+router.post('/login',async (req,res) => {
+    //TODO: validation
+
+    const {email,password} = req.body;
+
+    const user= await User.findOne({ email });
+
+    if(user && (await brcypt.compare(password,user.password))) {
+        // Create a new jwt token
+        const token = jwt.sign({user_id: user._id, email}, "testset",{expiresIn: "1h"});
+
+        user.token = token;
+
+        //TODO: reponse builder
+        res.json({ email, token });
+        return;
+    }
+    res.status(400).json({
+        status: false,
+        message: "Invalid credentials"
+    })
 });
 
 module.exports = router;
