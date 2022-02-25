f63cec7a3e5a4d2718e2608f46dbfcb606084653
Metin Ağaoğlu <metnagaoglu@gmail.com>
2022-02-25 18:04:37 +0300

    Content filter has been added and integrated to whole request.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const filter = require('content-filter')
 
 // db connection
 const db = require('./database/connection')();
 
-var urlencodedParser = bodyParser.urlencoded({ extended: false })
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
 app.use(urlencodedParser);
 
 /**
@@ -19,19 +20,29 @@ const verifyToken = require('./middleware/verify-token');
 const authRouter = require('./routes/auth');
 const indexRouter = require('./routes/index');
 
+/**
+ * By this default using, content-filter checks the request URL for { and $ characters and functions and objects of the html body data property names for $ character coming by GET, POST, PUT and DELETE methods.
+ */
+const blackList = ['$', '{', '&&', '||']
+const options = {
+    dispatchToErrorHandler: true,
+    urlBlackList: blackList,
+    bodyBlackList: blackList
+}
+
+app.use(filter(options));
 app.use('/api',verifyToken);
 app.use('/api',indexRouter);
 app.use('/auth',authRouter);
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
     // set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
   
     res.status(err.status || 201);
-    res.json({ error: err.message });
-    
+    res.json({ status: false , error: err.message });
 });
 
 
